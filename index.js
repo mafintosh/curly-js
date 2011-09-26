72b41bb2444a09dadc6340188a1c2c1328c8bc52
Mathias Buus <mathiasbuus@gmail.com>
2011-09-27 01:43:31 +0200

    added jsonp support

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ Request.prototype.timeout = function(ms, callback) {
 };
 Request.prototype.destroy = noop;
 Request.prototype.query = function(query, callback) {
-	this._query = '?'+querify(query);
+	this._query = querify(query);
+	this._query = (this._query && '?') + this._query;
 	return this._short(callback);
 };
 Request.prototype.form = function(data, callback) {
@@ -138,6 +139,109 @@ Request.prototype._decode = function(data) {
 	return data;
 };
 
+var prefix = (new Date()).getTime().toString(36);
+var globalScope = window._tmp_jsonp = {}; // A global variable to reference jsonp closures
+var cnt = 0;
+
+var JSONP = function(url) {
+	url = url.split('?');
+
+	this._url = url[0];
+	this._query = url.slice(1).join('?') || '';
+};
+
+JSONP.prototype.timeout = Request.prototype.timeout; // exactly the same
+JSONP.prototype.query = Request.prototype.query; // exactly the same
+
+JSONP.prototype.strict = function(callback) {
+	this._strict = true;
+	return this._short(callback);	
+};
+JSONP.prototype.send = function(method, callback) {
+	if (!callback && typeof method !== 'string') {
+		callback = method;
+		method = 'callback';
+	}
+	callback = callback || noop;
+
+	var self = this;
+	var match = this._query.match(/(^|&)([^=])+=\?/);
+	var id = 'cb'+prefix+(cnt++).toString(36);
+	var callbackId = '_tmp_jsonp.'+id;
+	var ended = false;
+
+	if (match) {
+		this._query = this._query.replace(match[2]+'=?', match[2]+'='+callbackId);
+	} else {
+		this._query += (this._query ? '&' : '?') + method+'='+callbackId;
+	}
+
+	var url = this._url + this._query;
+
+	var onresult = function(err, result) {
+		ended = true;
+
+		var el = document.getElementById(id);
+		
+		if (el) {
+			el.onreadystatechange = noop;
+			el.parentNode.removeChild(el);
+		}
+		el = null; // no mem leaks
+
+		delete globalScope[id];
+
+		self.destroy = noop;
+
+		callback(err, result);
+	};
+
+	globalScope[id] = function(result) {
+		if (self._strict && (result === undefined || result === null || result.error)) {
+			var err = new Error((result && result.error) || 'result is undefined or null');
+			
+			if (result) {
+				for (var i in result) {
+					err[i] = result[i];
+				}
+			}
+			onresult(err);
+			return;
+		}
+		onresult(null, result);
+	};
+
+	if (document.body) {
+		var el = document.createElement('script');
+
+		el.async = true;
+		el.src = url;
+		el.id = id;
+
+		document.body.appendChild(el);
+		el = null; // no leaks
+	} else {
+		document.write(unescape('%3Cscript')+' src="'+url+'" id="'+id+'"'+unescape('%3E%3C/script%3E'));
+	}
+
+	document.getElementById(id).onreadystatechange = function() {
+		if (ended || (this.readyState !== 'loaded' && this.readyState !== 'complete')) {
+			return;
+		}
+		onresult(new Error('jsonp request failed'));								
+	};
+	
+	this.destroy = function() {
+		onresult(new Error('jsonp request was cancelled'));
+	};
+
+	return this;
+};
+JSONP.prototype.destroy = noop;
+
+JSONP.prototype._short = Request.prototype._short; // exactly the same
+
+
 var methods = {'POST':0, 'GET':0, 'DELETE':'del', 'PUT':0};
 
 var define = function(method) {
@@ -154,7 +258,15 @@ var define = function(method) {
 
 exports.cors = ('withCredentials' in new XMLHttpRequest());
 
+exports.jsonp = function(url, callback) {
+	var req = new JSONP(url);
+
+	if (callback) {
+		req.send(callback);
+	}
+	return req;
+};
+
 for (var method in methods) {
 	exports[(methods[method] || method).toLowerCase()] = define(method);
 }
-
